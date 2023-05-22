d12ab8abd3b0e847fbb3871c4d12f13ae34e7f12
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-21 20:30:54 -0500

    Add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,18 @@ import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import '@/styles/globals.css'
 import { lightTheme } from '../../themes'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme} >
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <ThemeProvider theme={lightTheme} >
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,16 +4,9 @@ export default function Document() {
   return (
     <Html lang="es-co">
       <Head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
       </Head>
       <body>
