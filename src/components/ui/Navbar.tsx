4978a129fe622d95e36e6c39dd4c1783581bb09d
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-22 18:33:02 -0500

    Update Navbar with Tailwind

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,76 +1,38 @@
-import NextLink from "next/link";
-import { AppBar, Badge, Box, Button, IconButton, Link, Toolbar, Typography } from '@mui/material';
-import { Notifications } from "@mui/icons-material";
+import Link from "next/link";
+import SearchIcon from "../icons/SearchIcon";
 
 export const Navbar = () => {
   return (
-    <AppBar>
-      <Toolbar>
-        <NextLink href='/' passHref legacyBehavior>
-          <Link display='flex' alignItems='center' sx={{ color: '#000000de' }}>
-            <Typography variant="h6">Zaiko |</Typography>
-            <Typography variant="h6" sx={{ ml: 0.5 }}>Software</Typography>
-          </Link>
-        </NextLink>
-
-        <Box flex={1} />
-
-        <Box sx={{ display: { xs: 'none', sm: 'none', md: 'block' } }}>
-          {/* <NextLink href='/clientes' passHref legacyBehavior>
-            <Link>
-              <Button>Clientes</Button>
-            </Link>
-          </NextLink> */}
-          <NextLink href='/productos' passHref legacyBehavior>
-            <Link>
-              <Button>Productos</Button>
-            </Link>
-          </NextLink>
-          <NextLink href='/ventas' passHref legacyBehavior>
-            <Link>
-              <Button>Ventas</Button>
-            </Link>
-          </NextLink>
-          {/* <NextLink href='/compras' passHref legacyBehavior>
-            <Link>
-              <Button>Compras</Button>
-            </Link>
-          </NextLink> */}
-          {/* <NextLink href='/proveedores' passHref legacyBehavior>
-            <Link>
-              <Button>Proveedores</Button>
-            </Link>
-          </NextLink> */}
-          <NextLink href='/admin' passHref legacyBehavior>
-            <Link>
-              <Button>Administración</Button>
-            </Link>
-          </NextLink>
-          {/* <NextLink href='/reportes' passHref legacyBehavior>
-            <Link>
-              <Button>Reportes</Button>
-            </Link>
-          </NextLink> */}
-        </Box>
-
-        <Box flex={1} />
-
-        <NextLink href='/notificaciones' passHref legacyBehavior>
-          <Link>
-            <IconButton>
-              <Badge badgeContent={2} color="secondary">
-                <Notifications />
-              </Badge>
-            </IconButton>
-          </Link>
-        </NextLink>
-
-        <Button>
+    <nav className="flex items-center top-0 fixed bg-white h-[60px] w-full px-6">
+      <section className="flex flex-grow justify-start w-auto whitespace-nowrap">
+        <Link href="/" >
+          <h2>
+            <span className="text-xl font-medium">Zaiko |</span>
+            <span className="text-xl ml-2">Software</span>
+          </h2>
+        </Link>
+      </section>
+      <section className="flex flex-grow">
+        <div className="hidden sm:block">
+          <a href="/customers" className="btn-nav">
+            Clientes
+          </a>
+          <a href="/products" className="btn-nav">
+            Productos
+          </a>
+          <a href="/inventory" className="btn-nav">
+            Inventario
+          </a>
+        </div>
+      </section>
+      <section className="flex flex-grow justify-end">
+        <button className="btn-nav-icon">
+          <SearchIcon width={24} height={24} />
+        </button>
+        <button className="btn-nav">
           Menú
-        </Button>
-
-      </Toolbar>
-    </AppBar>
-
+        </button>
+      </section>
+    </nav>
   )
 }
