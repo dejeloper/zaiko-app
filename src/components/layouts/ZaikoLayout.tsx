16f92de3517cc5ad0e8a745e2fd4045bf35594c2
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-21 20:33:25 -0500

    Add Zaiko Layout

--- src/components/layouts/ZaikoLayout.tsx
+++ src/components/layouts/ZaikoLayout.tsx
@@ -1,6 +1,8 @@
 import { FC, ReactNode } from "react";
 import Head from "next/head"
 
+import { Navbar } from "../ui";
+
 interface Props {
   children: ReactNode;
   title: string;
@@ -12,11 +14,13 @@ export const ZaikoLayout: FC<Props> = ({ children, title, pageDescription }) => 
     <>
       <Head>
         <title>{title}</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="description" content={pageDescription} />
       </Head>
 
       <nav>
-
+        <Navbar />
       </nav>
 
       <main style={{ margin: '80px', maxWidth: '1440px', padding: '0px 30px' }}>
