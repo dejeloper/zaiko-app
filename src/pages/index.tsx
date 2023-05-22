6f4adfd3f9746be361c4b44846f2bedb2da0e6a1
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-22 18:33:34 -0500

    Update Index Page with Tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,36 @@
 import { NextPage } from "next";
-import { Typography } from "@mui/material";
 
 import { ZaikoLayout } from "@/components/layouts";
+import { menuInitial } from "@/settings/menu";
+
 
 const Home: NextPage = () => {
   return (
     <ZaikoLayout title={"Zaiko - Inicio"} pageDescription={"Este es la página de Inicio"}>
-      <Typography variant="h1" component="h1">Inicio</Typography>
-      <Typography variant="h2" sx={{ mb: 1 }} >Todos los productos</Typography>
+      <h2 className="text-3xl font-semibold">
+        Inicio
+      </h2>
+      <h4 className="text-md font-normal mb-4">Hola, ¿En qué vamos a trabajar hoy?</h4>
+      <section id="cards" className="grid grid-flow-row grid-cols-3 gap-8 w-full">
+        {
+          menuInitial.menusHome.map((menu) => (
+            <article key={menu.id}>
+              <header className="rounded-md border border-black/1 shadow-md shadow-black/20 flex justify-center items-center">
+                <button>
+                  <picture>
+                    <img src={`images/menus/${menu.image}`} alt={menu.descriptionMenu} style={{ height: '160px' }} />
+                  </picture>
+                </button>
+              </header>
+              <footer className=" py-4 px-2 flex justify-center items-center">
+                <span className="font-bold font-ro">
+                  {menu.name}
+                </span>
+              </footer>
+            </article>
+          ))
+        }
+      </section>
     </ZaikoLayout>
   )
 }
