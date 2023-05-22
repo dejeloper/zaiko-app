0b686ddb3efe07c702763632f75c60c2931fac40
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-22 18:33:52 -0500

    Update Page 404 with Tailwind

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,16 @@
 import { ZaikoLayout } from "@/components/layouts"
-import { Box, Typography } from "@mui/material"
 
 const Custom404 = () => {
   return (
     <ZaikoLayout title='Página no Encontrada' pageDescription='Página no encontrada - Página 404' >
-      <Box
-        display='flex'
-        justifyContent='center'
-        alignItems='center'
-        height='calc(100vh - 200px)'
-        sx={{ flexDirection: { sm: 'column', md: 'row' } }}
-      >
-        <Typography variant='h1' component='h1' fontSize={60} fontWeight={200}>404 |</Typography>
-        <Typography marginLeft={2} fontSize={20} >No encontramos ninguna página</Typography>
-      </Box>
+      <section className="flex justify-center items-center" style={{ height: 'calc(100vh - 200px)' }}>
+        <h1 className="text-3xl">
+          404 |
+        </h1>
+        <h2 className="ml-2 text-2xl font-light">
+          No encontramos ninguna página
+        </h2>
+      </section>
     </ZaikoLayout>
   )
 }
