578a6c50ebf4e2c4d8c4d2cf5e4998bc09bc5b79
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-22 18:32:25 -0500

    Remove MUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,5 @@
 import type { AppProps } from 'next/app'
-import { CssBaseline, ThemeProvider } from '@mui/material'
 import '@/styles/globals.css'
-import { lightTheme } from '../../themes'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -10,10 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <ThemeProvider theme={lightTheme} >
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   )
 }
