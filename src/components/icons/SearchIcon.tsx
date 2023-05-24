d55e891369bb2b165880fc7082cbab70f8b19b70
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-23 21:06:16 -0500

    Update SearchIcon

--- src/components/icons/SearchIcon.tsx
+++ src/components/icons/SearchIcon.tsx
@@ -4,16 +4,18 @@ import { SVGProps } from "react"
 const SearchIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    width={props.width}
+    height={props.height}
     fill="none"
     stroke="currentColor"
     strokeLinecap="round"
     strokeLinejoin="round"
-    strokeWidth={2}
+    strokeWidth="2"
     className="icon icon-tabler icon-tabler-search"
-    {...props}
+    viewBox="0 0 24 24"
   >
-    <path stroke="none" d="M0 0h24v24H0z" />
-    <path d="M3 10a7 7 0 1 0 14 0 7 7 0 1 0-14 0M21 21l-6-6" />
+    <path stroke="none" d="M0 0h24v24H0z"></path>
+    <path d="M3 10a7 7 0 1014 0 7 7 0 10-14 0M21 21l-6-6"></path>
   </svg>
 )
 export default SearchIcon
