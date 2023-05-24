d4a4d06a825c1b61b58d4461d41a7eb99bf8cff7
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-23 21:54:07 -0500

    Fix Menu Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 
 import { ZaikoLayout } from "@/components/layouts";
-import { menuInitial } from "@/settings/menu";
+import { menuHome } from "@/settings/menu";
 import Link from "next/link";
 
 
@@ -14,18 +14,18 @@ const Home: NextPage = () => {
       <h4 className="text-md font-normal mb-4">Hola, ¿En qué vamos a trabajar hoy?</h4>
       <section id="cards" className="grid grid-flow-row xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 w-full">
         {
-          menuInitial.menusHome.map((menu) => (
-            <article key={menu.id}>
+          menuHome.map(({ id, name, image, descriptionMenu, url }) => (
+            <article key={id}>
               <header className="rounded-md border border-black/1 shadow-md shadow-black/20 flex justify-center items-center">
-                <Link href={menu.url}>
+                <Link href={url}>
                   <picture>
-                    <img src={`images/menus/${menu.image}`} alt={menu.descriptionMenu} style={{ height: '160px' }} />
+                    <img src={`images/menus/${image}`} alt={descriptionMenu} style={{ height: '160px' }} />
                   </picture>
                 </Link>
               </header>
               <footer className=" py-4 px-2 flex justify-center items-center">
                 <span className="font-bold font-ro">
-                  {menu.name}
+                  {name}
                 </span>
               </footer>
             </article>
