0f78c395cc067041865caa0ecf91172a31194602
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-24 23:44:22 -0500

    Add Producto Services for testing

---
+++ src/services/products.ts
@@ -0,0 +1,167 @@
+import { Product, } from '../interfaces/products';
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'Producto 1',
+    category: 'Bebidas Calientes',
+    inStock: -1,
+    price: 100000,
+    size: ['Pequeño', 'Mediano', 'Grande', 'ExtraGrande'],
+    image: 'product_1.png'
+  },
+  {
+    id: 2,
+    name: 'Producto 2',
+    category: 'Bebidas Calientes',
+    inStock: -1,
+    price: 100000,
+    size: ['Grande', 'ExtraGrande'],
+    image: 'product_2.png'
+  },
+  {
+    id: 3,
+    name: 'Producto 3',
+    category: 'Bebidas Calientes',
+    inStock: -1,
+    price: 100000,
+    size: ['Pequeño', 'ExtraGrande'],
+    image: 'product_3.png'
+  },
+  {
+    id: 4,
+    name: 'Producto 4',
+    category: 'Bebidas Calientes',
+    inStock: -1,
+    price: 100000,
+    size: ['Grande'],
+    image: 'product_4.png'
+  },
+  {
+    id: 5,
+    name: 'Producto 5',
+    category: 'Bebidas frías',
+    inStock: -1,
+    price: 100000,
+    size: ['Pequeño', 'Mediano', 'Grande'],
+    image: 'product_5.png'
+  },
+  {
+    id: 6,
+    name: 'Producto 6',
+    category: 'Bebidas frías',
+    inStock: -1,
+    price: 100000,
+    size: ['Pequeño', 'Mediano'],
+    image: 'product_6.png'
+  },
+  {
+    id: 7,
+    name: 'Producto 7',
+    category: 'Bebidas frías',
+    inStock: -1,
+    price: 100000,
+    size: ['Mediano'],
+    image: 'product_7.png'
+  },
+  {
+    id: 8,
+    name: 'Producto 8',
+    category: 'Bebidas frías',
+    inStock: -1,
+    price: 100000,
+    size: ['Mediano', 'Grande'],
+    image: 'product_8.png'
+  },
+  {
+    id: 9,
+    name: 'Producto 9',
+    category: 'Bebidas frías',
+    inStock: -1,
+    price: 100000,
+    size: ['Pequeño', 'Mediano', 'Grande', 'ExtraGrande'],
+    image: 'product_9.png'
+  },
+  {
+    id: 10,
+    name: 'Producto 10',
+    category: 'Bebidas frías',
+    inStock: -1,
+    price: 100000,
+    size: ['Grande', 'ExtraGrande'],
+    image: 'product_10.png'
+  },
+  {
+    id: 11,
+    name: 'Producto 11',
+    category: 'Hojaldres',
+    inStock: -1,
+    price: 100000,
+    size: ['Mediano', 'Grande'],
+    image: 'product_11.png'
+  },
+  {
+    id: 12,
+    name: 'Producto 12',
+    category: 'Hojaldres',
+    inStock: -1,
+    price: 100000,
+    size: ['Mediano'],
+    image: 'product_12.png'
+  },
+  {
+    id: 13,
+    name: 'Producto 13',
+    category: 'Hojaldres',
+    inStock: -1,
+    price: 100000,
+    size: ['Mediano'],
+    image: 'product_13.png'
+  },
+  {
+    id: 14,
+    name: 'Producto 14',
+    category: 'Hojaldres',
+    inStock: -1,
+    price: 100000,
+    size: ['Pequeño', 'Mediano'],
+    image: 'product_14.png'
+  },
+  {
+    id: 15,
+    name: 'Producto 15',
+    category: 'Sanduches',
+    inStock: -1,
+    price: 100000,
+    size: ['Mediano'],
+    image: 'product_15.png'
+  },
+  {
+    id: 16,
+    name: 'Producto 16',
+    category: 'Licores',
+    inStock: -1,
+    price: 100000,
+    size: ['Mediano', 'Grande', 'ExtraGrande'],
+    image: 'product_16.png'
+  },
+  {
+    id: 17,
+    name: 'Producto 17',
+    category: 'Licores',
+    inStock: -1,
+    price: 100000,
+    size: ['Grande', 'ExtraGrande'],
+    image: 'product_17.png'
+  },
+  {
+    id: 18,
+    name: 'Producto 18',
+    category: 'Wafles',
+    inStock: -1,
+    price: 100000,
+    size: ['Grande'],
+    image: 'product_18.png'
+  },
+
+]
