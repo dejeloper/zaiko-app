7fca09f0db6f9ec8ef5b501f8aa47c73953aea49
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-06-04 00:33:33 -0500

    Add screen configuration to tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xxs': '360px',
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
