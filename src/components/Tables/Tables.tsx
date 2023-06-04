5d0847528382e1d30d129facf7574085c51518d7
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-06-04 00:36:16 -0500

    Add SalesTable component for sales

---
+++ src/components/Tables/Tables.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+
+interface Props {
+  id: number
+}
+
+export const Tables: FC<Props> = ({ id }) => {
+  return (
+    <div className="m-4">
+      {
+        id &&
+        <div className="flex flex-col justify-center items-center mx-auto bg-green-700 rounded-md border border-black w-[80px] h-[80px]">
+          <span className="">{id.toString()}</span>
+        </div>
+      }
+    </div>
+
+  )
+}
