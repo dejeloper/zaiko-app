10ef7898fa187fa629a9e844a4cdf582774ea309
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-06-06 21:27:35 -0500

    Update logic and styles for current table and zone change

--- src/components/sales/Tables.tsx
+++ src/components/sales/Tables.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import { SalesTableList } from "../SalesTable/SalesTableList"
+import 'remixicon/fonts/remixicon.css'
 
-const listZones = [
+const listZones: IZone[] = [
   {
     id: 0,
     name: 'Todas',
@@ -20,102 +21,146 @@ const listZones = [
   },
 ]
 
-const listTables = [
+const listTables: ITable[] = [
   {
     id: 1,
     textInformation: "Información 1",
-    zone: 'Sala 1',
+    zone: 1,
+    capacity: 4,
+    people: 0,
     enabled: true,
   },
   {
     id: 2,
     textInformation: "Información 2",
-    zone: 'Sala 1',
+    zone: 1,
+    capacity: 4,
+    people: 0,
     enabled: true,
   },
   {
     id: 3,
     textInformation: "Información 3",
-    zone: 'Sala 1',
+    zone: 1,
+    capacity: 4,
+    people: 0,
     enabled: true,
   },
   {
     id: 4,
     textInformation: "Información 4",
-    zone: 'Sala 1',
+    zone: 1,
+    capacity: 4,
+    people: 2,
     enabled: false,
   },
   {
     id: 5,
     textInformation: "Información 5",
-    zone: 'Sala 1',
+    zone: 1,
+    capacity: 4,
+    people: 2,
     enabled: false,
   },
   {
     id: 11,
     textInformation: "Información 1",
-    zone: 'Sala 2',
+    zone: 2,
+    capacity: 4,
+    people: 0,
     enabled: true,
   },
   {
     id: 12,
     textInformation: "Información 2",
-    zone: 'Sala 2',
+    zone: 2,
+    capacity: 4,
+    people: 0,
     enabled: true,
   },
   {
     id: 13,
     textInformation: "Información 3",
-    zone: 'Sala 2',
+    zone: 2,
+    capacity: 4,
+    people: 0,
     enabled: true,
   },
   {
     id: 14,
     textInformation: "Información 4",
-    zone: 'Sala 2',
+    zone: 2,
+    capacity: 4,
+    people: 2,
     enabled: false,
   },
   {
     id: 15,
     textInformation: "Información 5",
-    zone: 'Sala 2',
+    zone: 2,
+    capacity: 4,
+    people: 2,
     enabled: false,
   },
   {
     id: 21,
     textInformation: "Información 1",
-    zone: 'Sala Exterior',
+    zone: 3,
+    capacity: 4,
+    people: 0,
     enabled: true,
   },
   {
     id: 22,
     textInformation: "Información 2",
-    zone: 'Sala Exterior',
+    zone: 3,
+    capacity: 4,
+    people: 0,
     enabled: true,
   },
   {
     id: 23,
     textInformation: "Información 3",
-    zone: 'Sala Exterior',
+    zone: 3,
+    capacity: 4,
+    people: 0,
     enabled: true,
   },
   {
     id: 24,
     textInformation: "Información 4",
-    zone: 'Sala Exterior',
+    zone: 3,
+    capacity: 4,
+    people: 2,
     enabled: false,
   },
   {
     id: 25,
     textInformation: "Información 5",
-    zone: 'Sala Exterior',
+    zone: 3,
+    capacity: 4,
+    people: 2,
     enabled: false,
   },
 ]
 
+interface IZone {
+  id: number;
+  name: string
+}
+
+interface ITable {
+  id: number;
+  textInformation: string,
+  zone: number,
+  capacity: number;
+  people: number,
+  enabled: boolean,
+}
+
 export const Tables = () => {
-  const [textInformation, setTextInformation] = useState("")
-  const [zoneCurrent, setZoneCurrent] = useState("Sala 1")
+  const [tableCurrent, setTableCurrent] = useState<ITable>(null!)
+  const [zoneCurrent, setZoneCurrent] = useState(0)
   let countTable = 0;
   let countTableEnabled = 0;
   let countTableDisabled = 0;
@@ -130,12 +175,11 @@ export const Tables = () => {
     })
   }
 
-  // const changeZone = (event) => {
-  //   debugger
-  //   const values = event.target.value;
-  //   console.log(values);
-  //   // setZoneCurrent()
-  // }
+  const changeZone = (e: ChangeEvent<HTMLSelectElement>) => {
+    const current = Number(e.target.value);
+    setZoneCurrent(current)
+    setTableCurrent(null!)
+  }
 
   conteo();
 
@@ -148,16 +192,14 @@ export const Tables = () => {
           <div className="grid xxs:grid-cols-2 md:grid-cols-3 justify-center text-center border border-black">
             {
               listTables.map((t) => {
-                debugger
-                if (t.zone === zoneCurrent || zoneCurrent === "Todas") {
+                if (t.zone === zoneCurrent || zoneCurrent === 0) {
                   return (
                     // <SalesTable key={t.id} id={t.id} />
                     <div className="m-4" key={t.id}>
                       {
                         t.id &&
                         <button onClick={() => {
-                          setTextInformation(t.textInformation)
-
+                          setTableCurrent(t)
                         }}>
                           <div className={`flex flex-col justify-center items-center mx-auto rounded-md border border-black w-[80px] h-[80px] ${t.enabled ? 'bg-green-500' : 'bg-red-400'}`}>
                             {t.id.toString()}
@@ -177,16 +219,48 @@ export const Tables = () => {
           <h4 className="tab-content-title">Información General</h4>
           <section className="tab-content-body">
             <h3 className="tab-content-subtitle">Zonas</h3>
-            <select className="my-4" onChange={e => setZoneCurrent(e.target.value)} defaultValue={zoneCurrent} >
+            <select className="my-4 p-4 text-base" onChange={(e) => { changeZone(e) }
+            } defaultValue={zoneCurrent} >
               {
                 listZones.map((zone) => {
                   return (
-                    <option value={zone.name} key={zone.id}  >{zone.name}</option>
+                    <option value={zone.id} key={zone.id}  >{zone.name}</option>
                   )
                 })
               }
             </select>
           </section>
+          {
+            tableCurrent &&
+            (
+              <>
+                <h4 className="tab-content-title">Selección</h4>
+                <section className="border border-black rounded tab-content-body p-2">
+                  <p className="tab-content-subtitle">Mesa
+                    <span className="ml-2 font-semibold">{tableCurrent.id}</span>
+                  </p>
+                  <p className="tab-content-subtitle">
+                    {
+                      tableCurrent.enabled
+                        ? <i className={`ri-calendar-check-fill text-xl mr-2`}></i>
+                        : <i className={`ri-calendar-close-fill text-xl mr-2`}></i>
+                    }
+
+                    Estado
+                    <span className="ml-2 font-semibold">{tableCurrent.enabled ? 'Disponible' : 'Ocupada'}</span>
+                  </p>
+                  <p className="tab-content-subtitle">
+                    <i className={`ri-user-fill text-xl mr-2`}></i>
+                    Capacidad
+                    <span className="ml-2 font-semibold">
+                      {`${tableCurrent.people}/${tableCurrent.capacity}`}
+                    </span>
+                  </p>
+
+                </section>
+              </>
+            )
+          }
         </div>
       </div>
     </div>
